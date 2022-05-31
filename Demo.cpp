25e9c75b23278f4529b39c93918e29370ba725c1
prakharrai2512 <prakharrai2512@gmail.com>
2022-05-31 12:27:56 +0530

    done

--- Demo.cpp
+++ Demo.cpp
@@ -8,44 +8,51 @@ int main() {
     torch::jit::script::Module module1;
     torch::jit::script::Module module2;
     torch::DeviceType device_type;
-    device_type = torch::kCUDA;
+    device_type = torch::kCPU;
     std::cout << torch::cuda::is_available() << std::endl;
 
     try {
         // Deserialize the ScriptModule from a file using torch::jit::load().
-        module1 = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\torchik_gpu.pt");
+        module1 = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\blaziken.pt");
         module1.to(device_type);
     }
     catch (const c10::Error& e) {
         std::cerr << "error loading the model\n"<<e.what();
         return -1;
     }
+
+    torch::jit::script::Module data;
+
     try {
         // Deserialize the ScriptModule from a file using torch::jit::load().
-        module2 = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\combusken_gpu.pt");
-        module2.to(device_type);
+        data = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\data_blaze.pth");
+        module1.to(device_type);
     }
     catch (const c10::Error& e) {
-        std::cerr << "error loading the model again\n";
+        std::cerr << "error loading the model\n" << e.what();
         return -1;
     }
-    torch::jit::Module data = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\data_gpu.pth");
     //data.to(device_type);
-    auto img = data.attr("img").toTensor().to(at::kCUDA);
-    auto val_det = data.attr("det_out").toTuple()->elements()[0].toTensor().to(at::kCUDA);
-    auto da_det = data.attr("da_seg_out").toTensor().to(at::kCUDA);
-    auto ll_det = data.attr("ll_seg_out").toTensor().to(at::kCUDA);
+    auto img = data.attr("img").toTensor().to(device_type);
+    auto val_det = data.attr("det_out").toTensor().to(device_type);
+    auto da_det = data.attr("da_seg_out").toTensor().to(device_type);
+    auto ll_det = data.attr("ll_seg_out").toTensor().to(device_type);
 
     std::cout << "Beginning inference\n";
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
-    auto det_out =  module2.forward({ img }).toTuple()->elements()[0].toTensor().to(at::kCUDA);
+    /*auto det_out = module2.forward({img}).toTuple()->elements()[0].toTensor().to(at::kCUDA);
 
     auto maskout = module1.forward({ img }).toTensorVector();
-    //std::cout << maskout;
+
     auto ll_out = maskout[1].to(at::kCUDA);
     auto da_out = maskout[0].to(at::kCUDA);
+    */
+    auto retTup = module1.forward({ img }).toTuple()->elements();
+    auto da_out = retTup[0].toTensor().to(device_type);
+    auto ll_out = retTup[1].toTensor().to(device_type);
+    auto det_out = retTup[2].toTensor().to(device_type);
 
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
