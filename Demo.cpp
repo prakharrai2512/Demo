e226241dd2f8c2d1a44f299627696051aa408b6e
prakharrai2512 <prakharrai2512@gmail.com>
2022-05-31 16:24:36 +0530

    done

--- Demo.cpp
+++ Demo.cpp
@@ -2,42 +2,45 @@
 
 
 int main() {
-	torch::Tensor tensor = torch::rand({ 2, 3 }).to(at::kCUDA);
+	torch::Tensor tensor = torch::rand({ 2, 3 }).to(torch::kCUDA);
 	std::cout << tensor << std::endl;
 	std::cout << "Hello\n";
     torch::jit::script::Module module1;
     torch::jit::script::Module module2;
     torch::DeviceType device_type;
-    device_type = torch::kCPU;
+    device_type = torch::kCUDA;
     std::cout << torch::cuda::is_available() << std::endl;
-
+    std::cout << torch::cuda::device_count() << std::endl;
     try {
         // Deserialize the ScriptModule from a file using torch::jit::load().
-        module1 = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\blaziken.pt");
-        module1.to(device_type);
+        module1 = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\blaziken_gpu.pt",device_type);
+        //module1.to(device_type);
     }
     catch (const c10::Error& e) {
         std::cerr << "error loading the model\n"<<e.what();
         return -1;
     }
 
-    torch::jit::script::Module data;
+    torch::jit::script::Module data,data1;
 
     try {
         // Deserialize the ScriptModule from a file using torch::jit::load().
-        data = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\data_blaze.pth");
-        module1.to(device_type);
+        data = torch::jit::load("C:\\Users\\acer\\Desktop\\SA lab\\Demo\\data\\data_blaze_gpu.pth",torch::kCPU);
+        //data.to(device_type);
     }
     catch (const c10::Error& e) {
         std::cerr << "error loading the model\n" << e.what();
         return -1;
     }
+
     //data.to(device_type);
     auto img = data.attr("img").toTensor().to(device_type);
-    auto val_det = data.attr("det_out").toTensor().to(device_type);
-    auto da_det = data.attr("da_seg_out").toTensor().to(device_type);
-    auto ll_det = data.attr("ll_seg_out").toTensor().to(device_type);
 
+
+    auto val_det = data.attr("det_out").toTensor();
+    auto da_det = data.attr("da_seg_out").toTensor();
+    auto ll_det = data.attr("ll_seg_out").toTensor();
+    //std::cout << img<<std::endl;
     std::cout << "Beginning inference\n";
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
@@ -49,10 +52,10 @@ int main() {
     auto ll_out = maskout[1].to(at::kCUDA);
     auto da_out = maskout[0].to(at::kCUDA);
     */
-    auto retTup = module1.forward({ img }).toTuple()->elements();
-    auto da_out = retTup[0].toTensor().to(device_type);
-    auto ll_out = retTup[1].toTensor().to(device_type);
-    auto det_out = retTup[2].toTensor().to(device_type);
+    //torch::jit::Stack input = { img1 };
+    
+    auto retTup = module1.forward({ img }).toTupleRef();
+   
 
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
@@ -60,8 +63,19 @@ int main() {
     std::cout << "Iterations :" << 1 / elapsed_seconds.count() << " it/s\n";
     std::cout << "Done inference\n";
 
+    std::cout << "Waypoint 1 ";
+    auto da_out = retTup.elements()[0].toTensor().to(torch::kCPU);
+    std::cout << "Waypoint 2 ";
+    auto ll_out = retTup.elements()[1].toTensor().to(torch::kCPU);
+    std::cout << "Waypoint 3 ";
+    auto det_out = retTup.elements()[2].toTensor().to(torch::kCPU);
+    std::cout << "Waypoint 4\n";
+
     if (det_out.equal(val_det)) {
         std::cout << "Detection same\n";
+    }
+    else {
+        //std::cout << det_out - val_det;
     }
     if (ll_out.equal(ll_det)) {
         std::cout << "Lane Line same\n";
